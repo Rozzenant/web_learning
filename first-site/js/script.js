01bc819e1645d52dd07f3fd9d464dc5054dac6bf
Rozzenant <dmitry.pe4urckin@yandex.ru>
2023-08-28 03:07:14 +0300

    v0.41

--- first-site/js/script.js
+++ first-site/js/script.js
@@ -5,5 +5,62 @@ let previousOperation;
 const screen = document.querySelector(".screen")
 
 function buttonClick(value) {
-    
+    if(isNaN(value))
+        handleSymbol(value);
+    else
+        handleNumber(value);
+
+    screen.innerText = buffer;
 }
+
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        
+        case '=':
+            if (previousOperation === null)
+                return
+            mathOperation(parseInt(buffer));
+            previousOperation = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+
+        case '←':
+            if(buffer.length === 1)
+                buffer = '0';
+            else   
+                buffer = buffer.toString(0, buffer.length - 1);
+            break;
+
+        case '−':
+        case '×':
+        case '÷':
+        case '+':
+            handleMath(symbol);
+            break;
+    }  
+}
+
+function handleMath(symbol){
+    if (buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function(event){
+        buttonClick(event.target.innerText);
+    })
+}
+
+init();
